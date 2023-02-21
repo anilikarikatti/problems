e2d5d84074ba200bcac7bea386a6998c74ad63b4
anilikarikatti <anilkarikatti333@gmail.com>
2023-02-21 14:08:21 +0530

    new branch tried

---
+++ tables.js
@@ -0,0 +1,11 @@
+
+for(let i=1;i<11;i++){
+    let str = ''
+    for(let j = 2 ; j<10;j++){
+        // console.log(`${i} * ${j} = `+ i * j );
+        str+=`${j} * ${i} = `+ i * j  + '\t'
+    }
+    console.log(str);
+
+
+}
