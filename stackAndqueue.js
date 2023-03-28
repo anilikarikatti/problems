04bbb036db16d4619211fbaade9b7c7ad775a989
anilikarikatti <anilkarikatti333@gmail.com>
2023-03-28 11:24:47 +0530

    stack

---
+++ stackAndqueue.js
@@ -0,0 +1,33 @@
+/*
+let arr=[];
+
+
+for(let i=0;i<5;i++){
+    arr.push(i);   
+}
+
+for(let i=0;i<5;i++){
+   console.log( arr[i]); 
+}
+
+for(let i=0;i<5;i++){
+    let ele =arr.pop();
+    console.log(ele);
+}
+
+*/
+
+let arr=[];
+
+for(let i=0;i<5;i++){
+    arr.push(i);   
+}
+
+for(let i=0;i<5;i++){
+   console.log( arr[i]); 
+}
+
+for(let i=0;i<5;i++){
+    let ele =arr.shift();
+    console.log(ele);
+}
